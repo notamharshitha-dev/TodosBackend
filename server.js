b70c8070491927be8eccb35d80c72c320c773062
Harshitha <notamharshitha@gmail.com>
2026-06-14 15:47:07 +0530

    configured cors

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express=require("express");
 var mangoose=require("mongoose");
 const dns = require('dns');
 const cors=require("cors")
+app.use(cors())
 // Google DNS
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 mangoose.connect("mongodb+srv://harshitha:hello123@harshitha.vdue5eb.mongodb.net/?appName=Harshitha").then(()=>{
     console.log("connected succesfully")
 }).catch((err)=>{ console.log(err) })
-app.use(cors())
+
 var app=express();
 app.get("/",(req,res)=>{
     res.send({msg:"hello harshitha let us start backend for todos"})
